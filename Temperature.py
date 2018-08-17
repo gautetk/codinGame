f509ab80124825ee978f8d68701ba51efad3e2f1
Gaute Kolstad <gautekolstad@gmail.com>
2018-08-17 13:01:26 +0200

    start

---
+++ Temperature.py
@@ -0,0 +1,15 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(raw_input())  # the number of temperatures to analyse
+for i in raw_input().split():
+    # t: a temperature expressed as an integer ranging from -273 to 5526
+    t = int(i)
+
+# Write an action using print
+# To debug: print >> sys.stderr, "Debug messages..."
+
+print "result"
